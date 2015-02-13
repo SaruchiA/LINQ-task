6d59847caf4e6a04bb5911fdee8d8212d7892fc5
Saruchi Arora <arora.saruchi@gmail.com>
2015-02-13 18:08:22 +0530

    committed

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace LINQ_task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboEmployee = new System.Windows.Forms.ComboBox();
+            this.comboShift = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(257, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "showAvailability";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Employee details";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Shift details";
+            // 
+            // comboEmployee
+            // 
+            this.comboEmployee.FormattingEnabled = true;
+            this.comboEmployee.Location = new System.Drawing.Point(147, 58);
+            this.comboEmployee.Name = "comboEmployee";
+            this.comboEmployee.Size = new System.Drawing.Size(121, 21);
+            this.comboEmployee.TabIndex = 5;
+            // 
+            // comboShift
+            // 
+            this.comboShift.FormattingEnabled = true;
+            this.comboShift.Location = new System.Drawing.Point(442, 58);
+            this.comboShift.Name = "comboShift";
+            this.comboShift.Size = new System.Drawing.Size(121, 21);
+            this.comboShift.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 179);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 171);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(278, 179);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 171);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 430);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboShift);
+            this.Controls.Add(this.comboEmployee);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboEmployee;
+        private System.Windows.Forms.ComboBox comboShift;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LINQ_task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Employee employee1 = new Employee();
+            Shift shift1 = new Shift();
+            employee1.populateEmpDictionary();
+            shift1.populateShiftList();
+            comboEmployee.Items.AddRange(employee1.empDictionary.Values.ToArray());
+            for (int i = 0; i < shift1.shiftList.Count; i++)
+            {
+                if (!comboShift.Items.Contains(shift1.shiftList.ElementAt(i).Value.ToString()))
+                //if (shift1.shiftList .Contains 
+                comboShift.Items.Add(shift1.shiftList.ElementAt(i).Value.ToString());
+            }
+            
+
+        }        
+
+        private void button1_Click(object sender, EventArgs e)
+        {         
+                     
+        }
+
+ 
+    }
+
+    public class Employee
+    {
+        private int id;
+        private String name;
+        List<Employee> employees = new List<Employee>();
+        Employee emp = new Employee();
+        public void populateEmployees()
+        {
+            emp.id = 1;
+            emp.name = "kanu";
+            employees.Add(emp);
+
+            emp .id=2;
+            emp.name = "vandana";
+            employees.Add(emp);
+
+            emp.id = 3;
+            emp.name = "komal";
+            employees.Add(emp);
+
+            emp.id = 4;
+            emp.name = "aishwarya";
+            employees.Add(emp);
+
+            emp.id = 5;
+            emp.name = "ritika";
+            employees.Add(emp);
+
+            emp.id = 6;
+            emp.name = "nishchint";
+            employees.Add(emp);
+
+            emp.id = 7;
+            emp.name = "tabassum";
+            employees.Add(emp);
+
+            emp.id = 8;
+            emp.name = "john";
+            employees.Add(emp);
+
+            emp.id = 9;
+            emp.name = "jane";
+            employees.Add(emp);
+
+            emp.id = 10;
+            emp.name = "ria";
+            employees.Add(emp);
+
+            emp.id = 11;
+            emp.name = "kanika";
+            employees.Add(emp);
+                        
+        }
+
+    }
+
+    public class Shift
+    {
+        private int id;
+        private String dayOfWeek;
+        List <Shift >=new List <Shift >();
+        Shift shift1 = new Shift();
+        
+        public void populateShifts()
+        {
+            shift1.id = 1;
+            shift1.dayOfWeek = "monday";
+            s
+        }
+       
+    }
+
+
+    /*------------------------------- Approach 1:- without using objects ----------------------------------*/
+
+    //class Employee
+    //{
+    //    private int id;
+    //    private String name;
+    //    public Dictionary<int, String> empDictionary = new Dictionary<int, String>();
+    //    public void populateEmpDictionary()
+    //    {
+    //        empDictionary.Add(1, "vandana");
+    //        empDictionary.Add(2, "komal");
+    //        empDictionary.Add(3, "aishwarya");
+    //        empDictionary.Add(4, "ritika");
+    //        empDictionary.Add(5, "nishchint");
+    //        empDictionary.Add(6, "tabassum");
+    //        empDictionary.Add(7, "john");
+    //        empDictionary.Add(8, "jane");
+    //        empDictionary.Add(9, "ria");
+    //        empDictionary.Add(10, "kanika");
+    //    }
+
+    //}
+
+
+    //class Shift
+    //{
+
+    //    public List<KeyValuePair<string, int>> shiftList = new List<KeyValuePair<string, int>>();
+    //    public void populateShiftList()
+    //    {
+    //        shiftList.Add(new KeyValuePair<string, int>("monday", 1));
+    //        shiftList.Add(new KeyValuePair<string, int>("monday", 2));
+    //        shiftList.Add(new KeyValuePair<string, int>("monday", 3));
+    //        shiftList.Add(new KeyValuePair<string, int>("tuesday", 4));
+    //        shiftList.Add(new KeyValuePair<string, int>("tuesday", 5));
+    //        shiftList.Add(new KeyValuePair<string, int>("tuesday", 6));
+    //        shiftList.Add(new KeyValuePair<string, int>("wednesday", 7));
+    //        shiftList.Add(new KeyValuePair<string, int>("wednesday", 8));
+    //        shiftList.Add(new KeyValuePair<string, int>("wednesday", 9));
+    //        shiftList.Add(new KeyValuePair<string, int>("thursday", 10));
+    //        shiftList.Add(new KeyValuePair<string, int>("thursday", 1));
+    //        shiftList.Add(new KeyValuePair<string, int>("thursday", 2));
+    //        shiftList.Add(new KeyValuePair<string, int>("friday", 3));
+    //        shiftList.Add(new KeyValuePair<string, int>("friday", 4));
+    //        shiftList.Add(new KeyValuePair<string, int>("friday", 5));
+    //        shiftList.Add(new KeyValuePair<string, int>("saturday", 6));
+    //        shiftList.Add(new KeyValuePair<string, int>("saturday", 7));
+    //        shiftList.Add(new KeyValuePair<string, int>("saturday", 8));
+    //        shiftList.Add(new KeyValuePair<string, int>("sunday", 9));
+    //        shiftList.Add(new KeyValuePair<string, int>("sunday", 10));
+    //        shiftList.Add(new KeyValuePair<string, int>("sunday", 1));
+
+    //    }
+
+    // }
+}
